f49cf5958a34847ca834cd624dcbb33924f1c268
scheidelg <scheidelg@gmail.com>
2020-03-28 11:58:28 -0400

    Update githubRetrieve.js

--- js/githubRetrieve.js
+++ js/githubRetrieve.js
@@ -1,48 +1,44 @@
-function githubRetrieve(form, authOnlyFlag) {
-    // 1
-    const login = form.username || form.querySelector('#login').value;
-    const password = form.token || form.querySelector('#password').value;
+function githubRetrieve(form) {
+  // 1
+  const login = form.username || form.querySelector('#login').value;
+  const password = form.token || form.querySelector('#password').value;
 
-    // 2
-    const token = btoa(`${login}:${password}`);
-    const request = new Request(
-        `https://api.github.com/repos/${org}/${repo}/contents/testfile?ref=${branch}`,
-        {
-            method: 'GET',
-            credentials: 'omit',
-            headers: {
-                Accept: 'application/json',
-                Authorization: `Basic ${token}`
-            },
-        }
-    );
+  // 2
+  const token = btoa(`${login}:${password}`);
+  const request = new Request(
+    `https://api.github.com/repos/${org}/${repo}/contents/testfile?ref=${branch}`,
+    {
+      method: 'GET',
+      credentials: 'omit',
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Basic ${token}`
+      },
+    });
 
-    // 3
-    fetch(request)
-    request.then(function (response) {
-        if (response.status !== 200) { // 4
-            document.querySelector('#loginForm').innerHTML = `Failed to load document (status: ${response.status})`;
-        } else {
-            response.json()
-            response.then(function (json) { // 5
-                localStorage.setItem('githubPagesAuth', JSON.stringify({ username: login, token: password }));
-
-                if (authOnlyFlag) {
-                    document.body.innerHTML = "Successful authentication by '" + login + "'.  Credentials saved for SSO."
-                } else {
-                    const content = json.encoding === 'base64' ? atob(json.content) : json.content;
+  // 3
+  fetch(request)
+    .then(function (response) {
+      if (response.status !== 200) { // 4
+        document.querySelector('#loginForm').innerHTML = `Failed to load document (status: ${response.status})`;
+      } else {
+        response.json()
+          .then(function (json) { // 5
+            const content = json.encoding === 'base64' ? atob(json.content) : json.content;
 
             // 6
-                    const startIdx = content.indexOf('<body');
-                    document.body.innerHTML = content.substring(
-                    content.indexOf('>', startIdx) + 1,
-                    content.indexOf('</body>'));
+
+            const startIdx = content.indexOf('<body');
+            document.body.innerHTML = content.substring(
+                content.indexOf('>', startIdx) + 1,
+                content.indexOf('</body>'));
 
 //            document.body.innerHTML = 'bob';
 //            document.write('fritz');
-                }
-            });
-        }
+
+            localStorage.setItem('githubPagesAuth', JSON.stringify({ username: login, token: password }));
+        });
+      }
     });
 
     return false;
