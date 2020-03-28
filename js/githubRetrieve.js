da8c9a3d9f5c63ac9eb9547828ec4989a28f44ab
scheidelg <scheidelg@gmail.com>
2020-03-28 12:14:13 -0400

    Update githubRetrieve.js

--- js/githubRetrieve.js
+++ js/githubRetrieve.js
@@ -23,22 +23,26 @@ function githubRetrieve(form, authOnlyFlag) {
             if (response.status !== 200) { // 4
                 document.querySelector('#loginForm').innerHTML = `Failed to load document (status: ${response.status})`;
             } else {
-                response.json().then(
-                    function (json) { // 5
-                        const content = json.encoding === 'base64' ? atob(json.content) : json.content;
+                if (authOnlyFlag) {
+                    document.body.innerHTML = "Successful authentication. Credentials saved for SSO."
+                } else {
+                    response.json().then(
+                        function (json) { // 5
+                            const content = json.encoding === 'base64' ? atob(json.content) : json.content;
 
-            // 6
-                        const startIdx = content.indexOf('<body');
-                        document.body.innerHTML = content.substring(
-                            content.indexOf('>', startIdx) + 1,
-                            content.indexOf('</body>'));
+                // 6
+                            const startIdx = content.indexOf('<body');
+                            document.body.innerHTML = content.substring(
+                                content.indexOf('>', startIdx) + 1,
+                                content.indexOf('</body>'));
 
-//            document.body.innerHTML = 'bob';
-//            document.write('fritz');
+    //            document.body.innerHTML = 'bob';
+    //            document.write('fritz');
 
-                        localStorage.setItem('githubPagesAuth', JSON.stringify({ username: login, token: password }));
-                    }
-                );
+                            localStorage.setItem('githubPagesAuth', JSON.stringify({ username: login, token: password }));
+                        }
+                    );
+                }
             }
         }
     );
