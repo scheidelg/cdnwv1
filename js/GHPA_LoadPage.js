a9432fce003d5e60e395d4cd3b8fd6b349e8dd09
scheidelg <scheidelg@gmail.com>
2020-03-29 16:52:59 -0400

    Update GHPA_LoadPage.js

--- js/GHPA_LoadPage.js
+++ js/GHPA_LoadPage.js
@@ -5,6 +5,6 @@ if (GHPA_SSO_Flag && GHPA_existingAuth) {
     fetch("/loginform.html").then(function (response) {
         return response.text();
     }).then(function (data) {
-        document.getElementById("loginForm").innerHTML = data;
+        document.getElementById("GHPA_loginForm").innerHTML = data;
     });
 }
