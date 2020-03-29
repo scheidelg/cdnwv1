9f12b61ae1014cf0971089a39bec31dc30c945d0
scheidelg <scheidelg@gmail.com>
2020-03-29 11:11:51 -0400

    Update githubRepositoryInfo.js

--- js/githubRepositoryInfo.js
+++ js/githubRepositoryInfo.js
@@ -2,6 +2,6 @@ const githubOrg = 'sans-blue-team';
 const githubRepo = 'cdnw2';
 const githubBranch = 'test';
 const githubDefaultHTMLfile = 'index.html';
-let githubSSO = true;
+let githubSSO = false;
 let githubFilename = '';
 let githubAuthOnlyFlag = false;
