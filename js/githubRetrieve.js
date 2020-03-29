7e20e3622f26bfb65acd7faee70c1d7c16efa30e
scheidelg <scheidelg@gmail.com>
2020-03-29 08:29:49 -0400

    Update githubRetrieve.js

--- js/githubRetrieve.js
+++ js/githubRetrieve.js
@@ -3,18 +3,36 @@ function githubRetrieve(form) {
     const login = form.username || form.querySelector('#login').value;
     const password = form.token || form.querySelector('#password').value;
 
-    /* Retrieve the pathname of the URL that is being accessed.  If the
-     * pathname is empty or ends with a '/' character, then append the default
-     * HTML file name that was set in the githubFilename variable, usually via
-     * the githubRepositoryInfo.js file.
+    /* The calling page can specify the page to load by setting a variable
+     * githubFilename.  If that variable exists, then just use it; otherwise
+     * retrieve the pathname of the URL for the current window. */
+    if (typeof githubFilename === 'undefined') {
+        let githubFilename = window.location.pathname;
+    }
+    
+    /* If the pathname for the file to retrieve is empty or ends with a '/'
+     * character, then append the default HTML file name that was set in the
+     * defaultHTMLfile variable (usually via the githubRepositoryInfo.js
+     * file).
      */
-    let githubFilename = (window.location.pathname == '' || window.location.pathname.slice(window.location.pathname.length -1) == '/') ? window.location.pathname + defaultHTMLfile : window.location.pathname
+    if (githubFilename == '' || githubFilename.slice(githubFilename.length -1) == '/') {
+        githubFilename = githubFilename + defaultHTMLfile
+    }
+    
+//githubFilename = (window.location.pathname == '' || window.location.pathname.slice(window.location.pathname.length -1) == '/') ? window.location.pathname + defaultHTMLfile : window.location.pathname
 
     /* If the filename begins with a '/' character then remove that character.
-     * Strictly speaking this shouldn't be necessary because the GitHub API
-     * is smart enough to deal with GET requests that contain '//' sequences.
-     * But, it's just a few lines of code and I think it's better to keep the
-     * GET request cleaner.
+     * Two notes:
+     *
+     *  - Every window.location.pathname should start with a '/' character, so
+     *    this will always match unless (a) a calling page specifically sets
+     *    the githubFilename variable without a leading '/', or (b) an edge
+     *    case in a browser.
+     *
+     *  - Strictly speaking this shouldn't be necessary because the GitHub API
+     *    is smart enough to deal with GET requests that contain '//'
+     *    sequences. But, it's just a few lines of code and I think it's
+     *    better to keep the GET request cleaner.
      */
     if (githubFilename.slice(0, 1) == '/') {
         githubFilename = githubFilename.slice(1)
@@ -39,25 +57,24 @@ let fritz='fritz';
 
     // 3
     fetch(request).then(function (response) {
-      if (response.status !== 200) { // 4
-        document.querySelector('#loginForm').innerHTML = `Failed to load GitHub document ${org} / ${repo} / ${githubFilename} (status: ${response.status})`;
-      } else {
-        response.json().then(function (json) { // 5
-            const content = json.encoding === 'base64' ? atob(json.content) : json.content;
+        if (response.status !== 200) { // 4
+            document.querySelector('#loginForm').innerHTML = `Failed to load GitHub document ${org} / ${repo} / ${githubFilename} (status: ${response.status})`;
+        } else {
+            response.json().then(function (json) { // 5
+                const content = json.encoding === 'base64' ? atob(json.content) : json.content;
 
-            // 6
-
-            const startIdx = content.indexOf('<body');
-            document.body.innerHTML = content.substring(
-                content.indexOf('>', startIdx) + 1,
-                content.indexOf('</body>'));
+                // 6
+                const startIdx = content.indexOf('<body');
+                document.body.innerHTML = content.substring(
+                    content.indexOf('>', startIdx) + 1,
+                    content.indexOf('</body>'));
 
 //            document.body.innerHTML = 'bob';
 //            document.write('fritz');
 
 //            localStorage.setItem('githubPagesAuth', JSON.stringify({ username: login, token: password }));
-        });
-      }
+            });
+        }
     });
 
     return false;
