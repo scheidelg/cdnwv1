b562e34cbda9e720e7070bd577f04f6f146a75d9
scheidelg <scheidelg@gmail.com>
2020-03-29 13:03:27 -0400

    Update githubSSO.js

--- js/githubSSO.js
+++ js/githubSSO.js
@@ -1,5 +1,6 @@
 const existingAuth = JSON.parse(localStorage.getItem('githubPagesAuth'));
 githubSSO = true;
 if (existingAuth) {
+    document.style.display="none";
     githubRetrieve(existingAuth);
 }
