5202037328bfe9e1048640e46f412b2983ebb3da
scheidelg <scheidelg@gmail.com>
2020-03-29 16:51:35 -0400

    Update GHPA_LoadPage.js

--- js/GHPA_LoadPage.js
+++ js/GHPA_LoadPage.js
@@ -1,6 +1,6 @@
 const GHPA_existingAuth = JSON.parse(localStorage.getItem('githubPagesAuth'));
 if (GHPA_SSO_Flag && GHPA_existingAuth) {
-    githubRetrieve(GHPA_existingAuth);
+    GHPA_Retrieve(GHPA_existingAuth);
 } else {
     fetch("/loginform.html").then(function (response) {
         return response.text();
