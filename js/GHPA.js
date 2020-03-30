25b03e32464480bb460ac1bd7389339d067a19b8
scheidelg <scheidelg@gmail.com>
2020-03-30 15:29:52 -0400

    Update GHPA.js

--- js/GHPA.js
+++ js/GHPA.js
@@ -1,8 +1,90 @@
-/* Load the public variables that configure the GHPA environment,
-             including the private GitHub organization, repository, and branch
-             from which we want to retrieve content.
+/*============================================================================
+GitHub Pages Authentication (GHP-Auth or GHPA)
 
-             Switch this over to a JSON configuration file later. */
+(blah blah blah)
+strip comments if you want, from this and sample HTML files
+ghpa.scheidel.net / github.io (whatever it is)
+----------------------------------------------------------------------------*/
+
+
+/*============================================================================
+Declare and define the public variables that configure the GHPA environment,
+including the private GitHub organization, repository, and branch from which
+we want to retrieve content.
+
+Switch this over to a JSON configuration file later.
+
+------------------------------------------------------------------------------
+Global variables are named starting with 'ghpa'.  Local variables aren't.
+------------------------------------------------------------------------------
+ghpaAuthOnlyFlag              boolean
+
+    Flag whether a web page that calls ghpaRetrieve() should just perform an
+    authentication check (e.g., authentication-only) or should load a page
+    from the private GitHub repository.  This is set globally in this file
+    but can be overridden within an individual web page, for example:
+
+        <head><script>ghpAuthOnlyFlag=true;</script></head>
+
+    True:  authentication-only
+    False: load a page from the private GitHub repository (recommended global
+           value)
+
+ghpaBranch                    const string
+
+    Name of the repository branch to use when accessing the private GitHub
+    repository.
+
+ghpaDefaultHTMLfile           const string
+
+    Name of the file to load if  ghpaRetrieve() is called with ghpaFileName
+    set to a directory name or from a web page that has a
+    window.location.pathname of a directory name.  In both cases, identified
+    by the name ending in a '/a' character.
+
+    Typically set to 'index.html'.
+
+ghpaOrg                       const string
+
+    Name of the organization to use when accessing the private GitHub
+    repository.
+
+ghpaRepo                      const string
+
+    Name of the repository to use when accessing the private GitHub
+    repository.
+
+ghpaSSOFlag                   boolean
+
+    Flag whether a web page that calls ghpaRetrieve() should just use and save
+    credentials to be used for single sign-on (SSO).  When SSO is used:
+
+     - after successful authentication to the private GitHub repository, an
+       authentication token is saved in memory
+
+     - when attempting to access the private GitHub repository, memory is
+       checked for a saved authentication token; if found, the token is used
+       instead of prompting the user for authentication credentials
+
+    This is set globally in this file but can be overridden within an
+    individual web page, for example:
+
+        <head><script>ghpSSOFlag=false;</script></head>
+
+    True:  SSO is in use (recommended global value)
+    False: SSO is not in use
+
+ghpaFilename                  string
+
+    The filename to retrieve from the private GitHub repository.  This can
+    optionally be set in the calling web page to specify that a specific
+    file should be retrieved instead of just using the
+    window.location.pathname of the current browser window.  For example:
+
+        <head><script>ghpaFilename='getthisfile.html';</script></head>
+
+    This is generally *not* set.
+----------------------------------------------------------------------------*/
 const ghpaOrg = 'sans-blue-team';
 const ghpaRepo = 'cdnw2';
 const ghpaBranch = 'test';
@@ -11,70 +93,143 @@ let ghpaSSOFlag = true;
 let ghpaFilename = '';
 let ghpaAuthOnlyFlag = false;
 
+
 /*============================================================================
+function ghpaClearSSO
+------------------------------------------------------------------------------
+Clear the authentication token from memory so that subsequent attempts to
+access the private GitHub repository require re-authentication.
+------------------------------------------------------------------------------
+Arguments: none
+------------------------------------------------------------------------------
+Return value: none
 ----------------------------------------------------------------------------*/
 function ghpaClearSSO() {
    localStorage.removeItem('ghpaToken');
 }
 
+
 /*============================================================================
+function ghpaLoadPage
+------------------------------------------------------------------------------
 Attempt to retrieve authentication credentials from memory and - if SSO is
 enabled - use them to retrieve content from the private GitHub repository.
 
-If SSO is enabled, authentication credentials can be retrieved from memory,
-and the private GitHub repository file can be retrieved; then the page content
-is replaced with the retrieved content.
+If:
+
+ - SSO is enabled,
+  - authentication credentials can be retrieved from memory, and
+  - the private GitHub repository file can be retrieved
+
+then the page content is replaced with the retrieved content.
 
 Otherwise, the login form is loaded and (if there was a failed attempt to load
 content) a status message displayed.
    
-This script file needs to be loaded within (ideally at the bottom of) the
-<body>; otherwise it will generate an error when it tries to modify elements
-within the <body> that haven't yet been loaded.
+This function needs to be called within the <body> of the calling web page;
+otherwise it will generate an error when it tries to modify elements within
+the <body> that haven't yet been loaded.  The recommended method is to use
+the 'onload' method.  For example:
+
+    <body onload='ghpaLoadPage()'>
+
+Another option is to run the script within - preferably at the bottom of - the
+<body>.  For example:
+
+    <body>
+        HTML goes here.
+        
+        <!-- ideally this should be at the bottom of the <body> -->
+        <script>ghpaLoadPage();</script>
+    </body>
+------------------------------------------------------------------------------
+Arguments: none
+------------------------------------------------------------------------------
+Return value: none
 ----------------------------------------------------------------------------*/
 function ghpaLoadPage() {
-
-/* Attempt to retrieve GitHub authentication credentials from localStorage.
- *
- * This is horribly insecure because any JavaScript on the page can access
- * localStorage.  This is only for testing / proof-of-concept purpsoes; I need
- * to (1) switch to sessionStorage and (2) use an encrypted storage
- * mechanism.
- *
- * Also need to enable support for OAuth. */
-const ghpaExistingAuth = JSON.parse(localStorage.getItem('ghpaToken'));
-
-/* If SSO is enabled and we have existing authentication credentials to use,
- * then attempt to retrieve content from the private GitHub repository. */
-if (!(ghpaSSOFlag && ghpaExistingAuth && ghpaRetrieve(ghpaExistingAuth))) {
-
-    /* If any of:
-     *  - SSO isn't enabled;
+    /* Attempt to retrieve GitHub authentication credentials from
+     * localStorage.
      *
-     *  - we don't have existing authentication credentials to use; or
+     * This is horribly insecure because any JavaScript on the page can access
+     * localStorage.  This is only for testing / proof-of-concept purpsoes; I
+     * need to (1) switch to sessionStorage and (2) use an encrypted storage
+     * mechanism.
      *
-     *  - SSO was enabled and we do have existing authentication credentials,
-     *    but we weren't able to retrieve content from the private GitHub
-     *    repository
-     *
-     * then make sure the element ghpaPrompt  is displayed and load the login
-     * form. */
-    
-    /* Enable display of the element ghpaPrompt.  In JavaScript, we can set
-     * an elements 'style.display' property back to it's default by setting it
-     * to a null string. */
-    document.getElementById("ghpaPrompt").style.display = "";
+     * Also need to enable support for OAuth. */
+    const ghpaExistingAuth = JSON.parse(localStorage.getItem('ghpaToken'));
 
-    /* Load the login form and replace the HTML of the element
-     * ghpaLoginForm. */
-    fetch("/loginform.html").then(function (response) {
-        return response.text();
-    }).then(function (data) {
-        document.getElementById("ghpaLoginForm").innerHTML = data;
-    });
+    /* If SSO is enabled and we have existing authentication credentials to
+     * use, then attempt to retrieve content from the private GitHub
+     * repository. */
+    if (!(ghpaSSOFlag && ghpaExistingAuth && ghpaRetrieve(ghpaExistingAuth))) {
+
+        /* If any of:
+         *  - SSO isn't enabled;
+         *
+         *  - we don't have existing authentication credentials to use; or
+         *
+         *  - SSO was enabled and we do have existing authentication
+         *    credentials, but we weren't able to retrieve content from the
+         *    private GitHub repository
+         *
+         * then make sure the element ghpaPrompt  is displayed and load the
+         * login form. */
+
+        /* Enable display of the element ghpaPrompt.  In JavaScript, we can
+         * set an elements 'style.display' property back to it's default by
+         * setting it to a null string. */
+        document.getElementById("ghpaPrompt").style.display = "";
+
+        /* Load the login form and replace the HTML of the element
+         * ghpaLoginForm. */
+        fetch("/loginform.html").then(function (response) {
+            return response.text();
+        }).then(function (data) {
+            document.getElementById("ghpaLoginForm").innerHTML = data;
+        });
+    }
 }
-}
 
+
+/*============================================================================
+function ghpaRetrieve(form)
+------------------------------------------------------------------------------
+Attempt to authenticate to and retrieve content from the private GitHub
+repository.
+
+If authentication is successful and ghpaSSOFlag is true, then save credentials
+in memory for later use.
+
+If content can be retrieved and ghpaAuthOnlyFlag is false, then update the
+content on the calling web page.
+
+If there are any errors, update HTML on the calling web page to display the
+error message.
+
+This function can be called either from ghpaLoadPage() or directly from a web
+page.  For the latter case, the most common use would be to call as part of
+the submit action on a form.  For example:
+
+    <form onsubmit="event.preventDefault(); ghpaRetrieve(this);">
+------------------------------------------------------------------------------
+Arguments
+
+form                          form element
+
+    The form element must have two input fields:
+
+     - An id of 'ghpaLogin' to hold the user's login name.
+
+     - An  id of 'ghpaPassword' to hold the user's password.
+------------------------------------------------------------------------------
+Return Value
+
+true:  received an HTML response code of 200 when retrieving the content
+       from the private GitHub repository
+
+false: did *not* receive an HTML response code of 200
+----------------------------------------------------------------------------*/
 function ghpaRetrieve(form) {
 
     let fetchResponse=0; // really the only reason we need this is so that we can do a final check at the end, outside of the scope of the fetch request
@@ -224,7 +379,7 @@ function ghpaRetrieve(form) {
         fetchResponse=response.status;
     });
 
-    /* We're calling this from one of two places:
+    /* We're generally calling this from one of two places:
      *
      *  (a) On submission of an HTML form where we've prevented the default
      *      form action from firing.
