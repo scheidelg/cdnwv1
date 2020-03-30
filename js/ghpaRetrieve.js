d308d3dcf262a10376f57a0a58291205d6b7cf21
scheidelg <scheidelg@gmail.com>
2020-03-30 08:03:04 -0400

    Update and rename GHPA_Retrieve.js to ghpaRetrieve.js

--- js/ghpaRetrieve.js
+++ js/ghpaRetrieve.js
@@ -1,28 +1,27 @@
-function GHPA_Retrieve(form) {
+function ghpaRetrieve(form) {
 
     /* Extract the login and password that were passed to this function
      * (either from the authentication form or retrieved from
      * localStorage). */
-    const login = form.username || form.querySelector('#GHPA_login').value;
-    const password = form.token || form.querySelector('#GHPA_password').value;
+    const login = form.username || form.querySelector('#ghpaLogin').value;
+    const password = form.token || form.querySelector('#ghpaPassword').value;
 
-    /* The GHPA_Filename variable is initially defined in the
-     * GHPA_Config.js file, and set to an emptry string.  The calling
-     * page can optionally specify the private page to load by setting the
-     * value of the variable.
+    /* The ghpaFilename variable is initially defined in the ghpaConfig.js
+     * file, and set to an emptry string.  The calling page can optionally
+     * specify the private page to load by setting the value of the variable.
      *
      * If the variable is set to an empty string then retrieve the pathname of
      * the URL for the current window; if the variable is set to a non-empty
      * string, the use the current value. */
-    if (GHPA_Filename === '') {
-        GHPA_Filename = window.location.pathname;
+    if (ghpaFilename === '') {
+        ghpaFilename = window.location.pathname;
     }
 
     /* If the pathname for the file to retrieve is empty or ends with a '/'
      * character, then append the default HTML file name that was set in the
-     * GHPA_DefaultHTMLfile variable (usually via the GHPA_Config.js file). */
-    if (GHPA_Filename == '' || GHPA_Filename.slice(GHPA_Filename.length -1) == '/') {
-        GHPA_Filename = GHPA_Filename + GHPA_DefaultHTMLfile
+     * ghpaDefaultHTMLfile variable (usually via the ghpaConfig.js file). */
+    if (ghpaFilename == '' || ghpaFilename.slice(ghpaFilename.length -1) == '/') {
+        ghpaFilename = ghpaFilename + ghpaDefaultHTMLfile
     }
     
     /* If the filename begins with a '/' character then remove that character.
@@ -30,15 +29,15 @@ function GHPA_Retrieve(form) {
      *
      *  - Every window.location.pathname should start with a '/' character, so
      *    this will always match unless (a) a calling page specifically sets
-     *    the GHPA_Filename variable without a leading '/', or (b) an edge
+     *    the ghpaFilename variable without a leading '/', or (b) an edge
      *    case in a browser.
      *
      *  - Strictly speaking this shouldn't be necessary because the GitHub API
      *    is smart enough to deal with GET requests that contain '//'
      *    sequences. But, it's just a few lines of code and I think it's
      *    better to keep the GET request cleaner. */
-    if (GHPA_Filename.slice(0, 1) == '/') {
-        GHPA_Filename = GHPA_Filename.slice(1)
+    if (ghpaFilename.slice(0, 1) == '/') {
+        ghpaFilename = ghpaFilename.slice(1)
     }
 
     /* Create the authentication token using the login and password that were
@@ -47,7 +46,7 @@ function GHPA_Retrieve(form) {
     
     // Craft the GitHub GET request to retrieve the specified file.
     const request = new Request(
-        `https://api.github.com/repos/${GHPA_Org}/${GHPA_Repo}/contents/${GHPA_Filename}?ref=${GHPA_Branch}`,
+        `https://api.github.com/repos/${ghpaOrg}/${ghpaRepo}/contents/${ghpaFilename}?ref=${ghpaBranch}`,
         {
             method: 'GET',
             credentials: 'omit',
@@ -67,24 +66,24 @@ function GHPA_Retrieve(form) {
         if (response.status == 200 || response.status == 404) {
             
             // if SSO is enabled, then store credentials
-            if (GHPA_SSO_Flag) {
+            if (ghpaSSOFlag) {
                 localStorage.setItem('githubPagesAuth', JSON.stringify({ username: login, token: password }));
             }
             
             /* If we're only performing an authentication check then display
                the appropriate message. */
-            if (GHPA_AuthOnlyFlag) {
+            if (ghpaAuthOnlyFlag) {
                 /* Updating document.getElementById("loginForm").innerHTML
                  * instead of document.body.innerHTML to avoid a Javascript
                  * error if the content wasn't successfully retrieved. */
-                 document.getElementById("GHPA_loginForm").innerHTML = `Successful GitHub authentication to ${GHPA_Org} / ${GHPA_Repo} / ${GHPA_Branch} by ${login}.` + (GHPA_SSO_Flag ? " Credentials saved for SSO." : "");
+                 document.getElementById("ghpaLoginForm").innerHTML = `Successful GitHub authentication to ${ghpaOrg} / ${ghpaRepo} / ${ghpaBranch} by ${login}.` + (ghpaSSOFlag ? " Credentials saved for SSO." : "");
             }            
         }
 
         /* If we successfully retrieved the contents and we are *not* only
          * performing an authentication check, then display the retrieved
          * content. */
-        if (response.status == 200 && ! GHPA_AuthOnlyFlag) {        
+        if (response.status == 200 && ! ghpaAuthOnlyFlag) {        
             response.json().then(function (json) { // 5
                 const content = json.encoding === 'base64' ? atob(json.content) : json.content;
 
@@ -100,11 +99,11 @@ function GHPA_Retrieve(form) {
         /* If we didn't successfully retrieve the content, and didn't get a
          * 'page not found' 404 error on a 'only perform an authenticate
          * check', then display an appropriate error message. */
-        if (response.status !=200 && !(response.status == 404 && GHPA_AuthOnlyFlag)) {
+        if (response.status !=200 && !(response.status == 404 && ghpaAuthOnlyFlag)) {
             /* Updating  document.getElementById("loginForm").innerHTML
              * instead of document.body.innerHTML to avoid a Javascript error
              * if the content wasn't successfully retrieved. */
-             document.getElementById("GHPA_loginForm").innerHTML = `Failed to load ${GHPA_Org} / ${GHPA_Repo} / ${GHPA_Branch} / ${GHPA_Filename} by ${login} (status: ${response.status}).`;
+             document.getElementById("ghpaLoginForm").innerHTML = `Failed to load ${ghpaOrg} / ${ghpaRepo} / ${ghpaBranch} / ${ghpaFilename} by ${login} (status: ${response.status}).`;
         }
     });
 
@@ -116,7 +115,7 @@ function GHPA_Retrieve(form) {
      *      In this case it doesn't really matter what we return from this
      *      function... but we should return *something.*
      *
-     *  (b) From GHPA_LoadPage.js when both SSO is enabled and authentication
+     *  (b) From ghpaLoadPage.js when both SSO is enabled and authentication
      *      credentials are in localStorage.
      *
      *      In this case we want to return true/false to identify whether
