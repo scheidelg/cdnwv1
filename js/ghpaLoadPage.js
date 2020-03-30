225ab613a2ef959bb57edea9c841f7bb0f36ba14
scheidelg <scheidelg@gmail.com>
2020-03-30 08:29:26 -0400

    Update ghpaLoadPage.js

--- js/ghpaLoadPage.js
+++ js/ghpaLoadPage.js
@@ -1,11 +1,13 @@
 const ghpaExistingAuth = JSON.parse(localStorage.getItem('ghpaToken'));
 
 if (!(ghpaSSOFlag && ghpaExistingAuth && ghpaRetrieve(ghpaExistingAuth))) {
+    document.getElementById("ghpaPrompt").style.display = "block";
+
     fetch("/loginform.html").then(function (response) {
         return response.text();
     }).then(function (data) {
         document.getElementById("ghpaLoginForm").innerHTML = data;
     });
     
-    document.getElementById("bobby").style.display = "block";
+    document.getElementById("ghpaAuthFailure").style.display = "block";
 }
