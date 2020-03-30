adbc127c1450552147cd07e8809cdcd2b32c7af6
scheidelg <scheidelg@gmail.com>
2020-03-30 07:07:25 -0400

    Update GHPA_LoadPage.js

--- js/GHPA_LoadPage.js
+++ js/GHPA_LoadPage.js
@@ -1,10 +1,11 @@
 const GHPA_existingAuth = JSON.parse(localStorage.getItem('githubPagesAuth'));
-if (GHPA_SSO_Flag && GHPA_existingAuth) {
-    GHPA_Retrieve(GHPA_existingAuth);
-} else {
+
+if (GHPA_SSO_Flag && GHPA_existingAuth && GHPA_Retreive(GHPA_existingAuth)) {
     fetch("/loginform.html").then(function (response) {
         return response.text();
     }).then(function (data) {
         document.getElementById("GHPA_loginForm").innerHTML = data;
     });
+    
+    document.getElementById("GHPA_prompt").style.display = "block";
 }
