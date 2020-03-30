17a179abaac2146db79fc7f16d20e72a850cb90f
scheidelg <scheidelg@gmail.com>
2020-03-30 07:56:24 -0400

    Update and rename GHPA_Config.js to ghpaConfig.js

--- js/GHPA_Config.js
+++
@@ -1,7 +0,0 @@
-const GHPA_Org = 'sans-blue-team';
-const GHPA_Repo = 'cdnw2';
-const GHPA_Branch = 'test';
-const GHPA_DefaultHTMLfile = 'index.html';
-let GHPA_SSO_Flag = true;
-let GHPA_Filename = '';
-let GHPA_AuthOnlyFlag = false;
